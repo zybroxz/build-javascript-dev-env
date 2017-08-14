066074e8941f777ad156bccbb7ae00863233c13d
Darren.Willoughby <dw@captecsystems.com>
2017-08-14 17:17:37 +0100

    Added some babel

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,6 @@
-var chalk = require("chalk");
+
+import chalk from "chalk"; //ES 2016 syntax
+
+//var chalk = require("chalk"); // ES 2015 syntax
 
 console.log(chalk.green("starting application"));
